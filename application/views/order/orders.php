36e24d828a7def423421a90c4fd09019f9440ee5
akbarharii <harimukti789@gmail.com>
2021-04-06 16:20:31 +0700

    Update UI Statis

--- application/views/order/orders.php
+++ application/views/order/orders.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <!-- Wrapper -->
 <div id="wrapper">
 	<div class="container">
@@ -23,66 +23,93 @@
 				</div>
 			</div>
 
+
+
 			<div class="col-sm-12 col-md-9">
 				<div class="row-custom">
 					<div class="profile-tab-content">
 						<!-- include message block -->
 						<?php $this->load->view('partials/_messages'); ?>
+						<!-- SEARCH -->
+						<div class="container">
+							<div class="row justify-content-end">
+								<div class="col-12 col-md-10 col-lg-8">
+									<form class="card card-sm">
+										<div class="card-body row no-gutters align-items-center">
+											<div class="col-auto">
+												<i class="fas fa-search h4 text-body"></i>
+											</div>
+											<!--end of col-->
+											<div class="col">
+												<input class="form-control form-control-borderless src_fild" type="search" placeholder="Cari Pesanan">
+											</div>
+											<!--end of col-->
+											<div class="col-auto">
+												<button class="btn btn-lg btn-primary src_btn" type="submit">Cari</button>
+											</div>
+											<!--end of col-->
+										</div>
+									</form>
+								</div>
+								<!--end of col-->
+							</div>
+						</div>
+						<!-- END OF SEARCH -->
 						<div class="table-responsive">
 							<table id="example" class="table table-striped">
 								<thead>
-								<tr>
-									<th scope="col"><?php echo trans("order"); ?></th>
-									<th scope="col"><?php echo trans("total"); ?></th>
-									<th scope="col"><?php echo trans("payment"); ?></th>
-									<th scope="col"><?php echo trans("status"); ?></th>
-									<th scope="col"><?php echo trans("date"); ?></th>
-									<th scope="col"><?php echo trans("options"); ?></th>
-								</tr>
+									<tr>
+										<th scope="col"><?php echo trans("order"); ?></th>
+										<th scope="col"><?php echo trans("total"); ?></th>
+										<th scope="col"><?php echo trans("payment"); ?></th>
+										<th scope="col"><?php echo trans("status"); ?></th>
+										<th scope="col"><?php echo trans("date"); ?></th>
+										<th scope="col"><?php echo trans("options"); ?></th>
+									</tr>
 								</thead>
 								<tbody>
-								<?php if (!empty($orders)): ?>
-									<?php foreach ($orders as $order): ?>
-										<tr>
-											<td>#<?php echo $order->order_number; ?></td>
-											<td><?php echo price_formatted($order->price_total, $order->price_currency); ?></td>
-											<td>
-												<?php if ($order->payment_status == 'payment_received'):
-													echo trans("payment_received");
-												else:
-													echo trans("awaiting_payment");
-												endif; ?>
-											</td>
-											<td>
-												<strong class="font-600">
-													<?php if ($order->payment_status == 'awaiting_payment'):
-														if ($order->payment_method == 'Cash On Delivery') {
-															echo trans("order_processing");
-														} else {
-															echo trans("awaiting_payment");
-														}
-													else:
-														if ($order->status == 1):
-															echo trans("completed");
-														else:
-															echo trans("order_processing");
-														endif;
+									<?php if (!empty($orders)) : ?>
+										<?php foreach ($orders as $order) : ?>
+											<tr>
+												<td>#<?php echo $order->order_number; ?></td>
+												<td><?php echo price_formatted($order->price_total, $order->price_currency); ?></td>
+												<td>
+													<?php if ($order->payment_status == 'payment_received') :
+														echo trans("payment_received");
+													else :
+														echo trans("awaiting_payment");
 													endif; ?>
-												</strong>
-											</td>
-											<td><?php echo formatted_date($order->created_at); ?></td>
-											<td>
-												<a href="<?php echo generate_url("order_details") . "/" . $order->order_number; ?>" class="btn btn-sm btn-table-info"><?php echo trans("details"); ?></a>
-											</td>
-										</tr>
-									<?php endforeach; ?>
-								<?php endif; ?>
+												</td>
+												<td>
+													<strong class="font-600">
+														<?php if ($order->payment_status == 'awaiting_payment') :
+															if ($order->payment_method == 'Cash On Delivery') {
+																echo trans("order_processing");
+															} else {
+																echo trans("awaiting_payment");
+															}
+														else :
+															if ($order->status == 1) :
+																echo trans("completed");
+															else :
+																echo trans("order_processing");
+															endif;
+														endif; ?>
+													</strong>
+												</td>
+												<td><?php echo formatted_date($order->created_at); ?></td>
+												<td>
+													<a href="<?php echo generate_url("order_details") . "/" . $order->order_number; ?>" class="btn btn-sm btn-table-info"><?php echo trans("details"); ?></a>
+												</td>
+											</tr>
+										<?php endforeach; ?>
+									<?php endif; ?>
 								</tbody>
 							</table>
 						</div>
 
 
-						<?php if (empty($orders)): ?>
+						<?php if (empty($orders)) : ?>
 							<p class="text-center">
 								<?php echo trans("no_records_found"); ?>
 							</p>
@@ -99,4 +126,3 @@
 	</div>
 </div>
 <!-- Wrapper End-->
-
--- application/views/partials/_header.php
+++ application/views/partials/_header.php
@@ -280,7 +280,7 @@
                 <div class="modal-content">
                     <div class="auth-box">
                         <button type="button" class="close" data-dismiss="modal"><i class="icon-close"></i></button>
-                        <h4 class="title"><?php echo trans("login"); ?></h4>
+                        <h4 class="title"><?php echo trans("login_as"); ?></h4>
                         <!-- Login As -->
                         <section class="py-2 bg-login-as py-2">
                             <div class="container">
@@ -334,25 +334,6 @@
                                                         <div class="col-xs-8 col-sm-7 col-md-8">
                                                             <h5>Pengawas</h5>
                                                             <p>Masuk Sebagai Pengawas</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="row mb-2">
-                                    <div class="col-md-12">
-                                        <a href="<?= base_url('admin/partners') ?>" class="text-dark">
-                                            <div class="card shadow">
-                                                <div class="card-body">
-                                                    <div class="row">
-                                                        <div class="col-xs-1 col-sm-2 col-md-1 mr-3">
-                                                            <i class="fa fa-tachometer fa-4x "></i></br>
-                                                        </div>
-                                                        <div class="col-xs-8 col-sm-7 col-md-8">
-                                                            <h5>Mitra</h5>
-                                                            <p>Masuk Sebagai Mitra</p>
                                                         </div>
                                                     </div>
                                                 </div>
