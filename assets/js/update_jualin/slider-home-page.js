cf06be8b165b4cfd6c0c672d568399b8fa50bf19
herlandroando <herlandrotri@gmail.com>
2021-04-06 10:43:28 +0700

    Add Slick to make slide content

--- assets/js/update_jualin/slider-home-page.js
+++ assets/js/update_jualin/slider-home-page.js
@@ -1,22 +1,25 @@
-$(document).ready(function() {
-    $('.customer-logos').slick({
+$(document).ready(function () {
+
+    $('.featured-categories-collection').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
+        arrows:true,
+        dots: true,
+        pauseOnHover: true,
         responsive: [{
-            breakpoint: 768,
+            breakpoint: 1024,
             settings: {
                 slidesToShow: 4
             }
         }, {
-            breakpoint: 520,
+            breakpoint: 700,
             settings: {
                 slidesToShow: 3
             }
         }]
     });
+
+
 });
